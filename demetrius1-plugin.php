9590b17f6637183c61b119c075bc81db8ebb4be4
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-07-24 15:50:22 +0100

    test static register

--- demetrius1-plugin.php
+++ demetrius1-plugin.php
@@ -36,33 +36,15 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-// 1. Precaution, in case someone unwanted tryies to access the file by exploiting a vulnerability (checks for one global variable)
-// if (!defined('ABSPATH')) {
-// 	die;
-// }
-
 // 2. Precaution, same as above (checks for one constant variable)
 defined('ABSPATH') or die("Hey, what are  you doing here? You silly human!");
-
-// 3. Precaution, same as above (checks for one function)
-// if (!function_exists('add_action')) {
-// 	echo "Hey, what are  you doing here? You silly human!";
-// 	exit;
-// }
-
 // Wordpress might reject the plugin if it doesn't have at least one of the 3 protections above
 
 class Demetrius1Plugin
 {
-	function __construct()
+	public static function register()
 	{
-		// add_action('init', array($this, 'custom_post_type'));
-		// die('reached end of __constructor');
-	}
-
-	function register()
-	{
-		add_action('admin_enqueue_scripts', array($this, 'enqueue')); // this uses the backend, for frontend replace "admin" with "wp"
+		add_action('admin_enqueue_scripts', array('Demetrius1Plugin', 'enqueue')); // this uses the backend, for frontend replace "admin" with "wp"
 	}
 
 	protected function create_post_type()
@@ -84,18 +66,12 @@ class Demetrius1Plugin
 		flush_rewrite_rules();
 	}
 
-	// function uninstall()
-	// {
-	// 	// delete the CPT
-	// 	// delete all the plugin data from the DB
-	// }
-
 	function custom_post_type()
 	{
 		register_post_type('book', ['public' => true, 'label' => 'Books']);
 	}
 
-	function enqueue()
+	static function enqueue()
 	{
 		// enqueue all our scripts
 		wp_enqueue_style('mypluginstyle', plugins_url('/assets/mystyle.css', __FILE__));
@@ -103,28 +79,14 @@ class Demetrius1Plugin
 	}
 }
 
-class SecondClass extends Demetrius1Plugin
-{
-	function register_post_type()
-	{
-		$this->create_post_type();
-	}
-}
-
 if (class_exists('Demetrius1Plugin')) {
-	$demetrius1Plugin = new Demetrius1Plugin();
-	$demetrius1Plugin->register();
+	// $demetrius1Plugin = new Demetrius1Plugin();
+	// $demetrius1Plugin->register();
+	Demetrius1Plugin::register();
 }
-
-$secondClass = new SecondClass();
-$secondClass->register_post_type();
-
 
 // activation
-register_activation_hook(__FILE__, array($demetrius1Plugin, 'activate'));
+// register_activation_hook(__FILE__, array($demetrius1Plugin, 'activate'));
 
 // deactivation
-register_deactivation_hook(__FILE__, array($demetrius1Plugin, 'deactivate'));
-
-// uninstall
-// register_uninstall_hook(__FILE__, array($demetrius1Plugin, 'uninstall'));
+// register_deactivation_hook(__FILE__, array($demetrius1Plugin, 'deactivate'));

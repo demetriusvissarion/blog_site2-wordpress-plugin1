f5dd63fb2b397c417aa1792a7fdd9717fff88b7f
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-07-25 12:38:00 +0100

    turned classes into services

--- demetrius1-plugin.php
+++ demetrius1-plugin.php
@@ -42,75 +42,9 @@ if (file_exists(dirname(__FILE__) . '/vendor/autoload.php')) {
 	require_once dirname(__FILE__) . '/vendor/autoload.php';
 }
 
-use Inc\Activate;
-use Inc\Deactivate;
-use Inc\Admin\AdminPages;
+define('PLUGIN_PATH', plugin_dir_path(__FILE__));
+define('PLUGIN_URL', plugin_dir_url(__FILE__));
 
-if (!class_exists('Demetrius1Plugin')) {
-	class Demetrius1Plugin
-	{
-		public $plugin;
-
-		function __construct()
-		{
-			$this->plugin = plugin_basename(__FILE__);
-		}
-
-		function register()
-		{
-			add_action('admin_enqueue_scripts', array($this, 'enqueue')); // this uses the backend, for frontend replace "admin" with "wp"
-
-			add_action('admin_menu', array($this, 'add_admin_pages'));
-
-			add_filter("plugin_action_links_$this->plugin", array($this, 'settings_link'));
-		}
-
-		public function settings_link($links)
-		{
-			$settings_link = '<a href="admin.php?page=demetrius1_plugin">Settings</a>';
-			array_push($links, $settings_link);
-			return $links;
-		}
-
-		public function add_admin_pages()
-		{
-			add_menu_page('Demetrius1 Plugin', 'Demetrius1', 'manage_options', 'demetrius1_plugin', array($this, 'admin_index'), 'dashicons-store', 110);
-		}
-
-		public function admin_index()
-		{
-			require_once plugin_dir_path(__FILE__) . 'templates/admin.php';
-		}
-
-		protected function create_post_type()
-		{
-			add_action('init', array($this, 'custom_post_type'));
-		}
-
-		function custom_post_type()
-		{
-			register_post_type('book', ['public' => true, 'label' => 'Books']);
-		}
-
-		function enqueue()
-		{
-			// enqueue all our scripts
-			wp_enqueue_style('mypluginstyle', plugins_url('/assets/mystyle.css', __FILE__));
-			wp_enqueue_script('mypluginscript', plugins_url('/assets/myscript.js', __FILE__));
-		}
-
-		function activate()
-		{
-			Activate::activate();
-		}
-	}
-
-	$demetrius1Plugin = new Demetrius1Plugin();
-	$demetrius1Plugin->register();
-
-	// activation
-	register_activation_hook(__FILE__, array($demetrius1Plugin, 'activate'));
-
-	// deactivation
-	register_deactivation_hook(__FILE__, array('Deactivate', 'deactivate'));
+if (class_exists('Inc\\Init')) {
+	Inc\Init::register_services();
 }
--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -8,7 +8,18 @@ namespace Inc\Pages;
 
 class Admin
 {
-	function __construct()
+	public function register()
 	{
+		add_action('admin_menu', array($this, 'add_admin_pages'));
+	}
+
+	public function add_admin_pages()
+	{
+		add_menu_page('Demetrius1 Plugin', 'Demetrius1', 'manage_options', 'demetrius1_plugin', array($this, 'admin_index'), 'dashicons-store', 110);
+	}
+
+	public function admin_index()
+	{
+		require_once PLUGIN_PATH . 'templates/admin.php';
 	}
 }

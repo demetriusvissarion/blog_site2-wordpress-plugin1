ee3c572047067ef8de339ed6444a996c628226b6
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-07-25 16:02:15 +0100

    modular admin page

--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -3,6 +3,7 @@
 namespace Inc\Pages;
 
 use \Inc\Base\BaseController;
+use \Inc\Api\SettingsApi;
 
 /**
  * @package Demetrius1Plugin
@@ -10,18 +11,38 @@ use \Inc\Base\BaseController;
 
 class Admin extends BaseController
 {
+	public $setting;
+
+	public function __construct()
+	{
+		$this->setting = new SettingsApi();
+	}
+
 	public function register()
 	{
-		add_action('admin_menu', array($this, 'add_admin_pages'));
+		// add_action('admin_menu', array($this, 'add_admin_pages'));
+		$pages = [[
+			'page_title' => 'Demetrius1 Plugin',
+			'menu_title' => 'Demetrius1',
+			'capability' => 'manage_options',
+			'menu_slug' => 'demetrius1_plugin',
+			'callback' => function () {
+				echo '<h1>Demetrius1 Plugin</h1>';
+			},
+			'icon_url' => 'dashicons-store',
+			'position' => 110,
+		]];
+
+		$this->setting->addPages($pages)->register();
 	}
 
-	public function add_admin_pages()
-	{
-		add_menu_page('Demetrius1 Plugin', 'Demetrius1', 'manage_options', 'demetrius1_plugin', array($this, 'admin_index'), 'dashicons-store', 110);
-	}
+	// public function add_admin_pages()
+	// {
+	// 	add_menu_page('Demetrius1 Plugin', 'Demetrius1', 'manage_options', 'demetrius1_plugin', array($this, 'admin_index'), 'dashicons-store', 110);
+	// }
 
-	public function admin_index()
-	{
-		require_once $this->plugin_path . 'templates/admin.php';
-	}
+	// public function admin_index()
+	// {
+	// 	require_once $this->plugin_path . 'templates/admin.php';
+	// }
 }
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Building a Wordpress plugin
+
+Full list of sections and features to build;
+
+- Modular Administration Area
+- Custom Post Type (CTP) Manager
+- Custom Taxonomy Manager
+- Widget to Upload and Display media in sidebars
+- Post and Pages Galery integration
+- Testimonial section: Comment in the front-end, Admins can approve comments, select which comments to dispaly
+- Custom template sections
+- Ajax based Login/Register system
+- Membership protected area
+- Chat system

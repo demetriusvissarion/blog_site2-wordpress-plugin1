8444d5096df79f165bc30c11e43a05e980a83426
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-07-25 13:10:23 +0100

    procedural methods

--- demetrius1-plugin.php
+++ demetrius1-plugin.php
@@ -46,6 +46,22 @@ define('PLUGIN_PATH', plugin_dir_path(__FILE__));
 define('PLUGIN_URL', plugin_dir_url(__FILE__));
 define('PLUGIN', plugin_basename(__FILE__));
 
+use Inc\Base\Activate;
+use Inc\Base\Deactivate;
+
+function activate_demetrius1_plugin()
+{
+	Activate::activate();
+}
+
+function deactivate_demetrius1_plugin()
+{
+	Deactivate::deactivate();
+}
+
+register_activation_hook(__FILE__, 'activate_demetrius1_plugin');
+register_deactivation_hook(__FILE__, 'deactivate_demetrius1_plugin');
+
 if (class_exists('Inc\\Init')) {
 	Inc\Init::register_services();
 }

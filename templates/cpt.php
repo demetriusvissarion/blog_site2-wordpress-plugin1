a2de6517a6297ba19bd9b5e5490c6be8872a5a24
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-07-28 16:11:06 +0100

    apply to dashboard

--- templates/cpt.php
+++ templates/cpt.php
@@ -1,1 +1,12 @@
-<h1>Custom Post Types Manager (2)</h1>
+<div class="wrap">
+	<h1>CPT Manager</h1>
+	<?php settings_errors(); ?>
+
+	<form method="post" action="options.php">
+		<?php
+		settings_fields('alecaddd_plugin_cpt_settings');
+		do_settings_sections('alecaddd_cpt');
+		submit_button();
+		?>
+	</form>
+</div>

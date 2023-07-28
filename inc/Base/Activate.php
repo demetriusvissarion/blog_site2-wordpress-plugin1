d9439f71b0e2526587818139657b06e56bb7528d
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-07-28 12:30:55 +0100

    set default options to disabled on activation

--- inc/Base/Activate.php
+++ inc/Base/Activate.php
@@ -11,5 +11,13 @@ class Activate
 	public static function activate()
 	{
 		flush_rewrite_rules();
+
+		if (get_option('demetrius1_plugin')) {
+			return;
+		}
+
+		$default = array();
+
+		update_option('demetrius1_plugin', $default);
 	}
 }

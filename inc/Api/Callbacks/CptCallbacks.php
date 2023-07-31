302aa8ed7d79642bece603779d0160512e79035b
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-07-31 12:38:50 +0100

    multiple items, store arrays in WP_Options

--- inc/Api/Callbacks/CptCallbacks.php
+++ inc/Api/Callbacks/CptCallbacks.php
@@ -16,20 +16,30 @@ class CptCallbacks
 
 	public function cptSanitize($input)
 	{
-		return $input;
+		$output = get_option('demetrius1_plugin_cpt');
+
+		foreach ($output as $key => $value) {
+			if ($input['post_type'] === $key) {
+				$output[$key] = $input;
+			} else {
+				$output[$input['post_type']] = $input;
+			}
+		}
+
+		return $output;
 	}
 
 	public function textField($args)
 	{
-		$value = '';   // DDV
+		// $value = '';   // DDV
 		$name = $args['label_for'];
 		$option_name = $args['option_name'];
 		$input = get_option($option_name);
-		if (!empty($input)) {   // DDV
-			$value = $input[$name];
-		}
+		// if (!empty($input)) {   // DDV
+		// 	$value = $input[$name];
+		// }
 
-		echo '<input type="text" class="regular-text" id="' . $name . '" name="' . $option_name . '[' . $name . ']" value="' . $value . '" placeholder="' . $args['placeholder'] . '">';
+		echo '<input type="text" class="regular-text" id="' . $name . '" name="' . $option_name . '[' . $name . ']" value="" placeholder="' . $args['placeholder'] . '">';
 	}
 
 	public function checkboxField($args)
@@ -38,8 +48,8 @@ class CptCallbacks
 		$classes = $args['class'];
 		$option_name = $args['option_name'];
 		$checkbox = get_option($option_name);
-		$checked = isset($checkbox[$name]) ? ($checkbox[$name] ? true : false) : false;
+		// $checked = isset($checkbox[$name]) ? ($checkbox[$name] ? true : false) : false;
 
-		echo '<div class="' . $classes . '"><input type="checkbox" id="' . $name . '" name="' . $option_name . '[' . $name . ']" value="1" class="" ' . ($checked ? 'checked' : '') . '><label for="' . $name . '"><div></div></label></div>';
+		echo '<div class="' . $classes . '"><input type="checkbox" id="' . $name . '" name="' . $option_name . '[' . $name . ']" value="1" class=""><label for="' . $name . '"><div></div></label></div>';
 	}
 }
